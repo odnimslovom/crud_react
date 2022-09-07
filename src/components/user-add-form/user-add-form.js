d3576135dcc81d3a8b089a1b600cc508b47063cd
Drew Paramonov <odnimslovom@icloud.com>
2022-09-07 09:42:51 +0300

    Simple validation of adding user

--- src/components/user-add-form/user-add-form.js
+++ src/components/user-add-form/user-add-form.js
@@ -20,11 +20,13 @@ class UserAddForm extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.onAdd(this.state.name, this.state.salary);
-        this.setState({
-            name: '',
-            salary: ''
-        })
+        if (this.state.name && this.state.salary) {
+            this.props.onAdd(this.state.name, this.state.salary);
+            this.setState({
+                name: '',
+                salary: ''
+            })
+        }
     }
 
 
